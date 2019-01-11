8a1c76b516785084392b8399caf6c9767aa8ea14
Antti Slunga <Antti.Slunga@solita.fi>
2019-01-11 10:16:56 +0200

    project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to re-frame-poc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/re_frame_poc/core.clj
@@ -0,0 +1,7 @@
+(ns re-frame-poc.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
